47d6534573c00337806e7dbd7cef3dd416847010
Álvaro Göede Rivera <alvaaz_@hotmail.com>
2018-10-08 16:20:41 -0300

    agrego graphql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { groupBy } from "lodash-es"
 import people from "./people"
 
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+    uri: 'https://api-useast.graphcms.com/v1/cjlwqnow603bo01gp1ub4hgoc/master'
+})
+
+
+
 import './style.scss'
 import './image-example'
 
@@ -10,3 +18,22 @@ const root = document.createElement("div")
 
 root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
 document.body.appendChild(root)
+
+import gql from 'graphql-tag';
+
+client.query({
+  query: gql`
+    query cars {
+      cars {
+        id
+        title
+        price
+        image {
+            handle
+        }
+      }
+    }
+  `,
+})
+  .then(data => console.log(data))
+  .catch(error => console.error(error));

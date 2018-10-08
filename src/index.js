b3813322130204d3e771a4fe874ce0931d367d9b
Álvaro Göede Rivera <alvaaz_@hotmail.com>
2018-10-08 16:59:03 -0300

    avances graphql

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import { groupBy } from "lodash-es"
 import people from "./people"
-
+import gql from 'graphql-tag'
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
     uri: 'https://api-useast.graphcms.com/v1/cjlwqnow603bo01gp1ub4hgoc/master'
 })
 
-
-
 import './style.scss'
 import './image-example'
 
-const managerGroups = groupBy(people, "manager")
+// const managerGroups = groupBy(people, "manager")
 
-const root = document.createElement("div")
+// const root = document.createElement("div")
 
-root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
-document.body.appendChild(root)
-
-import gql from 'graphql-tag';
+// root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
+// document.body.appendChild(root)
 
 client.query({
   query: gql`
@@ -35,5 +31,7 @@ client.query({
     }
   `,
 })
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+.then(data => console.log(data))
+.catch(error => console.error(error));
+
+
